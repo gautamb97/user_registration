9f54c4bcfb3010761d61710d4dc118b6119a1082
icode32 <www.gautam971997@gmail.com>
2020-11-05 15:29:28 +0530

    First

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "WELCOME FOR REGISTRATION";
+
+read -p "Enter your First Name " user_input;
+pattern="^[A-Za-z]{3,10}$"
+if [[ $user_input =~ $pattern ]];
+then
+        echo "valid";
+else
+        echo "invalid";
+fi

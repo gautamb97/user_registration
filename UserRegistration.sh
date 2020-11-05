9416173caa50d191e1458989811d8806511864e2
icode32 <www.gautam971997@gmail.com>
2020-11-05 18:02:26 +0530

    Last_Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,10 +3,18 @@
 echo "WELCOME FOR REGISTRATION";
 
 read -p "Enter your First Name " user_input;
-pattern="^[A-Za-z]{3,10}$"
+read -p "Enter your Last Name " last_name_input;
+pattern="^[A-Z]{1}[a-z]{2,10}$"
 if [[ $user_input =~ $pattern ]];
 then
-        echo "valid";
+        echo "Valid User Name";
 else
-        echo "invalid";
+        echo "invalid User Nmae";
 fi
+if [[ $last_name_input =~ $pattern ]];
+then
+        echo "Valid Last Name";
+else
+        echo "invalid Last Name";
+fi
+

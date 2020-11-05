1f231a6943472536562264dc8fc30893d1fab575
icode32 <www.gautam971997@gmail.com>
2020-11-05 22:32:36 +0530

    Adding_Mobile_Number

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -5,24 +5,33 @@ echo "WELCOME FOR REGISTRATION";
 read -p "Enter your First Name " user_input;
 read -p "Enter your Last Name " last_name_input;
 read -p "Enter your email address " email;
+read -p "Enter your Mobile Number With Country Code " mobile_number;
 pattern="^[A-Z]{1}[a-z]{2,10}$"
 pattern1="^[a-z0-9\.\_\-a-z0-9]*\@[a-z]*\.(com|org|in|co)$"
+pattern2="^\+[0-9]*\ [0-9]{10}$"
 if [[ $user_input =~ $pattern ]];
 then
-        echo "Valid User Name";
+        echo $user_input;
 else
         echo "invalid User Nmae";
 fi
 if [[ $last_name_input =~ $pattern ]];
 then
-        echo "Valid Last Name";
+        echo $last_name_input;
 else
         echo "invalid Last Name";
 fi
 
 if [[ $email =~ $pattern1 ]];
 then
-        echo "Valid Email Address";
+        echo $email;
 else
         echo "invalid Email Address";
 fi
+if [[ $mobile_number =~ $pattern2 ]];
+then
+        echo $mobile_number;
+else
+        echo "invalid Mobile Number";
+fi
+

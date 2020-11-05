ca315b64e949f029a8afa2f76fcd04812fc77afc
icode32 <www.gautam971997@gmail.com>
2020-11-05 22:09:08 +0530

    Email

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -4,7 +4,9 @@ echo "WELCOME FOR REGISTRATION";
 
 read -p "Enter your First Name " user_input;
 read -p "Enter your Last Name " last_name_input;
+read -p "Enter your email address " email;
 pattern="^[A-Z]{1}[a-z]{2,10}$"
+pattern1="^[a-z0-9\.\_\-a-z0-9]*\@[a-z]*\.(com|org|in|co)$"
 if [[ $user_input =~ $pattern ]];
 then
         echo "Valid User Name";
@@ -18,3 +20,9 @@ else
         echo "invalid Last Name";
 fi
 
+if [[ $email =~ $pattern1 ]];
+then
+        echo "Valid Email Address";
+else
+        echo "invalid Email Address";
+fi
